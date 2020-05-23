cd30a4b88ee6c9ef61a6925e5d5bcf525386ce63
chun37 <chun.jb.37@gmail.com>
2020-05-23 11:43:39 +0900

    webhookで送信したメッセージを削除するコマンド追加

--- cogs/amazon_link.py
+++ cogs/amazon_link.py
@@ -4,8 +4,16 @@ from urllib.parse import urlparse
 import aiohttp
 from discord import AsyncWebhookAdapter, Embed, Webhook
 from discord.ext import commands
+from discord.errors import NotFound, Forbidden
 
 AMAZON_URL_PATTERN = re.compile(r"https?://\S+?amazon\.co\.jp\S*?/dp/\S{10}\S*")
+MESSAGE_LINK_PATTERN = re.compile(
+    r"https?://.*?discordapp.com/channels/\d+/(\d+)/(\d+)"
+)
+
+
+class OwnershipError(Exception):
+    pass
 
 
 class AmazonShortLink(commands.Cog):
@@ -73,6 +81,44 @@ class AmazonShortLink(commands.Cog):
         await self.send_message(
             new_message, sender_name, sender_avatar_url, channel_webhook.url, embed
         )
+
+    async def delete_message_from_channel(self, ctx, channel, message_id):
+        try:
+            message = await channel.fetch_message(message_id)
+        except (Forbidden, NotFound):
+            raise OwnershipError("指定のメッセージがありません。\nこのチャンネルのメッセージでなければ、リンクで指定してください。")
+        if not message.embeds or len(message.embeds) > 1:
+            raise OwnershipError(f"{self.bot.user.name}に編集されたメッセージのみ削除することが出来ます。")
+        embed = message.embeds[0]
+        if not embed.footer:
+            raise OwnershipError(f"{self.bot.user.name}に編集されたメッセージのみ削除することが出来ます。")
+        footer_text = embed.footer.text
+        if footer_text != f"edited by {self.bot.user.name}, 発言者: {ctx.author.id}":
+            raise OwnershipError("他の人のメッセージを削除することは出来ません")
+
+        await message.delete()
+
+    @commands.command()
+    async def delete(self, ctx, link_or_id):
+        match_object = MESSAGE_LINK_PATTERN.search(link_or_id)
+
+        if match_object:
+            channel_id = match_object.group(1)
+            message_id = match_object.group(2)
+            try:
+                channel = await self.bot.fetch_channel(channel_id)
+            except (Forbidden, NotFound):
+                return await ctx.send("チャンネルが取得出来ませんでした。", delete_after=30)
+        elif link_or_id.isdecimal():
+            channel = ctx.channel
+            message_id = link_or_id
+        else:
+            return await ctx.send("メッセージIDかメッセージリンクを送ってね", delete_after=30)
+
+        try:
+            await self.delete_message_from_channel(ctx, channel, message_id)
+        except OwnershipError as e:
+            await ctx.send(*e.args, delete_after=30)
 
 
 def setup(bot):
