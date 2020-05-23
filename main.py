b57a7edc3fd971ee991d95c538c50b7040ea2efa
chun37 <chun.jb.37@gmail.com>
2020-05-23 11:47:35 +0900

    メンションで反応するようにした

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ INITIAL_COGS = [
 class MyBot(commands.Bot):
 
     def __init__(self):
-        super().__init__(command_prefix="?")
+        super().__init__(command_prefix=commands.when_mentioned_or("?"))
 
         for cog in INITIAL_COGS:
             try:
