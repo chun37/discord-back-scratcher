02fe3bd71e643865bc2f75503bd031829aa1ee45
chun37 <chun.jb.37@gmail.com>
2020-06-11 02:53:29 +0900

    絵文字情報をembedで見やすくした

--- cogs/sticker.py
+++ cogs/sticker.py
@@ -1,3 +1,4 @@
+from discord import Embed
 from discord.ext import commands
 
 
@@ -8,7 +9,17 @@ class Sticker(commands.Cog):
     @commands.command(aliases=["emoji", "sticker"])
     async def getCustomEmojiId(self, ctx, emoji: commands.EmojiConverter):
         """カスタム絵文字の情報を返します"""
-        await ctx.send(emoji.__repr__())
+        embed = Embed(title=emoji.name, url=str(emoji.url))
+        embed.set_thumbnail(url=str(emoji.url))
+        embed.add_field(
+            name="created_at",
+            value=emoji.created_at.strftime("%Y-%m-%d %H:%M:%S"),
+            inline=False,
+        )
+        embed.add_field(name="emoji_id", value=str(emoji.id), inline=False)
+        embed.add_field(name="guild_name", value=emoji.guild, inline=False)
+        embed.add_field(name="guild_id", value=str(emoji.guild_id), inline=False)
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
