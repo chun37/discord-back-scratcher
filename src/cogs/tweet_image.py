fcbd5d6c726690c4537d995f587526897bbdfb05
chun37 <chun.jb.37@gmail.com>
2020-08-26 09:43:33 +0900

    Twitter APIを使用して、2枚目以降の画像を取ってくるぞい

--- src/cogs/tweet_image.py
+++ src/cogs/tweet_image.py
@@ -1,29 +1,63 @@
+import os
 import re
-from typing import Any, Callable, List
+from typing import Any, Callable, List, Dict
+from dataclasses import dataclass
 
-import aiohttp
-import bs4
-from bs4 import BeautifulSoup as bs
+import tweepy
 from discord import Message
 from discord.ext.commands import Bot, Cog
+import typedjson
 
 TWEET_URL_PATTERN = re.compile(r"https?://twitter.com/\S+/status/\d+")
+
+
+def get_twitter_api() -> tweepy.API:
+    auth = tweepy.OAuthHandler(
+        os.environ["TWITTER_CONSUMER_KEY"], os.environ["TWITTER_CONSUMER_SECRET"]
+    )
+    auth.set_access_token(
+        os.environ["TWITTER_ACCESS_TOKEN"], os.environ["TWITTER_ACCESS_TOKEN_SECRET"]
+    )
+    api = tweepy.API(auth)
+    return api
+
+
+@dataclass(frozen=True)
+class Media:
+    id: int
+    id_str: str
+    indices: List[int]
+    media_url: str
+    media_url_https: str
+    url: str
+    display_url: str
+    expanded_url: str
+    type: str
+
+
+def get_serialized_media(json: Dict[str, Any]) -> Media:
+    obj: Media = typedjson.decode(Media, json)
+    return obj
 
 
 class TweetImage(Cog):
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
+        self.twitter_api = get_twitter_api()
 
     async def get_tweet_imageurls(self, url: str) -> List[str]:
-        headers = {
-            "User-Agent": "Mozilla/5.0 (compatible; Discordbot/2.0; +https://discordapp.com/)"
-        }
-        async with aiohttp.ClientSession(headers=headers) as session:
-            async with session.get(url) as res:
-                data = await res.text()
-        tags = bs(data, "lxml").select("meta[property=og\\:image]")
-        get_url: Callable[[bs4.element.Tag], Any] = lambda x: x["content"]
-        return list(map(get_url, tags))
+        tweet_id = url.split("/")[-1]
+        status = self.twitter_api.get_status(tweet_id)
+
+        # 2枚以上画像があると、extended_entitiesにデータが入る
+        if getattr(status, "extended_entities", None) is None:
+            print(f"2つ以上の画像が無いよ {tweet_id=}")
+            return []
+
+        serialized_media = map(get_serialized_media, status.extended_entities["media"])
+        get_url: Callable[[Media], str] = lambda x: x.media_url_https
+        photo_filter: Callable[[Media], bool] = lambda media: media.type == "photo"
+        return list(map(get_url, filter(photo_filter, serialized_media)))
 
     @Cog.listener()
     async def on_message(self, message: Message) -> None:
@@ -31,7 +65,7 @@ class TweetImage(Cog):
             return
         for url in TWEET_URL_PATTERN.findall(message.content):
             urls = await self.get_tweet_imageurls(url)
-            if len(urls) <= 1:
+            if not urls:
                 return
             await message.channel.send("2枚目以降の画像:\n" + "\n".join(urls[1:]))
         return
