f9bcead79c656bf1d3778e111da5a9cb1178d8d2
chun37 <chun.jb.37@gmail.com>
2020-09-26 21:18:25 +0900

    pylintに怒られるので、validationを消したｗ

--- custom/cog.py
+++ custom/cog.py
@@ -1,7 +1,6 @@
 import traceback
 from typing import Any, NoReturn, Optional
 
-import discord
 from discord import ClientUser, Guild, TextChannel
 from discord.ext.commands import BotMissingPermissions, Cog, Context
 
@@ -19,10 +18,6 @@ class CustomCog(Cog):
     def on_message_bot_has_permissions(
         guild: Guild, channel: TextChannel, bot_user: ClientUser, **perms: Any
     ) -> Optional[NoReturn]:
-        invalid = set(perms) - set(discord.Permissions.VALID_FLAGS)
-        if invalid:
-            raise TypeError("Invalid permission(s): %s" % (", ".join(invalid)))
-
         user = guild.me if guild is not None else bot_user
         permissions = channel.permissions_for(user)
 
